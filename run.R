c69ac01de1b4526081db9de47646f4780a97fcee
datallurgy <allyson@datallurgy.com>
2022-05-12 22:01:53 -0700

    add plotly to server.R

--- run.R
+++ run.R
@@ -2,10 +2,6 @@ library(shiny)
 
 port <- Sys.getenv("PORT")
 
-# Define working directory
-data <- read.table("PES2019.txt",header=TRUE,sep="\t")
-names(data)[1] <- "player"  #rename from "ï..player" to "player"
-
 shiny::runApp(
   appDir = getwd(),
   host = '0.0.0.0',
--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(plotly)
 library(scales)
 library(knitr)
 library(kableExtra)
