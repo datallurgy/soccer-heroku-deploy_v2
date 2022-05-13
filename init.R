93ffab410393de79ea99213bbf55eadd001ead38
datallurgy <allyson@datallurgy.com>
2022-05-12 21:06:54 -0700

    add shinydashboard to init.R

--- init.R
+++ init.R
@@ -1,3 +1,5 @@
+helpers.installPackages("shinydashboard")
+
 # Define working directory
 data <- read.table("PES2019.txt",header=TRUE,sep="\t")
 names(data)[1] <- "player"  #rename from "ï..player" to "player"
