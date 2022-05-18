b75e8b1cbb1ed7e6c3ec6591b58a000d29b97ffe
datallurgy <allyson@datallurgy.com>
2022-05-17 19:54:43 -0700

    workflow to contain secrets

--- src/create_credentials.R
+++ src/create_credentials.R
@@ -1,9 +1,13 @@
 library(shinymanager)
 
+user_pw <- Sys.getenv("USER_PW")
+admin_pw <- Sys.getenv("ADMIN_PW")
+passphrase <- Sys.getenv("PASSPHRASE")
+
 # Init DB using credentials data
 credentials <- data.frame(
   user = c("shiny", "shinymanager"),
-  password = c("qwerty", "12345"),
+  password = c(user_pw, admin_pw),
   admin = c(FALSE, TRUE),
   stringsAsFactors = FALSE
 )
@@ -17,5 +21,5 @@ create_db(
   credentials_data = credentials,
   sqlite_path = "data/database.sqlite", # will be created
   # passphrase = key_get("R-shinymanager-key", "obiwankenobi")
-  passphrase = "passphrase_without_keyring"
+  passphrase = passphrase
 )
